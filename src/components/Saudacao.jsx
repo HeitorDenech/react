387cb59f313a7260d683a32e519cabf75e21dfba
https://github.com/HeitorDenech <heitor.denech@gmail.com>
2024-06-26 15:57:27 -0300

    first commit

---
+++ src/components/Saudacao.jsx
@@ -0,0 +1,12 @@
+import "../style.css";
+
+function Saudacao({name}) {
+    return (
+
+        <h1 className="title">
+            Olá, {name}!
+        </h1>
+    ) 
+}
+
+export default Saudacao;
